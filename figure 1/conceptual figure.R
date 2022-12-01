ae12ab2cf3229536b5951117216f53955cfe822d
bblonder <bblonder@gmail.com>
2022-12-01 09:48:29 -0800

    fixed fig1

--- figure 1/conceptual figure.R
+++ figure 1/conceptual figure.R
@@ -7,17 +7,17 @@ el = as.matrix(data.frame(
   to=c("desired", "t1", "t2", "t3", "desired","o1","o2","o3","o4")))
 
 g = graph_from_edgelist(el)
-E(g)$weight =c(4,rep(1,4),rep(0.999,4))
+E(g)$type =c('direct',rep('shortcut',4),rep('brute-force',4))
 
 l = layout_nicely(g)
-l[2,1] = 1
+l[2,1] = 0.5
 
 pdf(file='g_illustration.pdf',width=5,height=5)
 plot(g,
-     edge.width=E(g)$weight, layout=l,
+     layout=l,
      vertex.label=NA,
      edge.arrow.size=0.75,
-     edge.color=ifelse(E(g)$weight==4,'black',ifelse(E(g)$weight==1,'black','red')),
+     edge.color=ifelse(E(g)$type=='direct','black',ifelse(E(g)$type=='shortcut','gray','red')),
      vertex.color=ifelse(V(g)$name=='initial','purple',
                          ifelse(V(g)$name=='desired','green',
                                 ifelse(grepl("^t",V(g)$name),'orange','lightgray'))),
